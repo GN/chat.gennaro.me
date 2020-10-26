9e7080c4aaab516c5b5c0b8358bb443b82177d5e
Gennaro Napolitano <gennaro@nau.edu>
2020-10-26 13:08:18 -0700

    minor changes

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -5,7 +5,7 @@ function Header() {
 
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="/#">Navbar</a>
+            <a className="navbar-brand" href="/#">GennChat</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
                     aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon" />
